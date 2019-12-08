637efdc0898f6a540482f2799fcb6a47e8feeae4
森幸浩 <mori-yukihiro@dmm.com>
2019-12-08 11:48:09 +0900

    Update index.js

--- functions/analyze_contents/index.js
+++ functions/analyze_contents/index.js
@@ -5,10 +5,27 @@ const pubsub = require('@google-cloud/pubsub');
 const vision = require('@google-cloud/vision');
 const bigquery = require('@google-cloud/bigquery');
 
-exports.analyzeContents = (data, context) => {
-  const file = data;
-  console.log(`File ${file.name} uploaded.`);
-  const jsonData = Buffer.from(data, 'base64').toString();
+exports.analyzeContents = async (data, context) => {
+  const object = data;
+  console.log(`File ${object.name} uploaded.`);
+  const jsonData = Buffer.from(object, 'base64').toString();
   var jsonObj = JSON.parse(jsonData);
   console.log(`Received name: ${jsonObj.name} and bucket: ${jsonObj.bucket} and contentType: ${jsonObj.contentType}`);
+
+  const file = storage.bucket(object.bucket).file(object.name);
+  const filePath = `gs://${object.bucket}/${object.name}`;
+
+  try {
+    const [result] = await vision.safeSearchDetection(filePath);
+    const detections = result.safeSearchAnnotation || {};
+
+    if (detections.adult === 'VERY_LIKELY') {
+      console.log(`Detected ${file.name} as inappropriate.`);
+    } else {
+      console.log(`Detected ${file.name} as OK.`);
+    }
+  } catch (err) {
+    console.error(`Failed to analyze ${file.name}.`, err);
+    throw err;
+  }
 };
